c418bbbd275c4986af3aed8bbebb19ffe80ad2f2
ganesh chaudhary <gchaudhary1995@gmail.com>
2020-09-22 17:12:20 +0530

    Add TextFeature and Font Selection

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
        <link href="css/bootstrap.min.css" rel="stylesheet">
 	   <!-- <link href="css/croppr.min.css" rel="stylesheet"/> -->
 	   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.9/cropper.css">
+
+	   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.7.1/jquery.contextMenu.min.css">
         <!-- Custom styles for this template -->
         <link href="css/style.css" rel="stylesheet">
 		<style>
@@ -26,8 +28,12 @@
 				background-repeat:no-repeat;
 				background-size:500px 500px;
 			}
-			.result{
-
+			.uploaded_images img{
+				width:80px;
+				height:80px;
+			}
+			.uploaded_images img.selected{
+				border-color:#40ca97;
 			}
 		</style>
 </head>
@@ -67,9 +73,12 @@
 							<label for="input file"></label>
                             <div class="custom-file">
 
-                                <input type="file" class="custom-file-input file2 input-set" data-image-selector=".image2"  id="customFile" name="filename" value="sample.fxdr">
+                                <input type="file" class="custom-file-input file2 input-set" data-image-selector=".image2"  id="customFile" name="filename" value="sample.fxdr" multiple>
                                 <label class="custom-file-label upload-set" for="customFile">DROP OR UPLOAD ARTWORK IMAGE HERE</label>
                             </div>
+						<div class="col-sm-12 uploaded_images shadow-sm" id="">
+
+						</div>
 						<div class="col-sm-6 col-6 d-none upload-image" id="uploaded_img">
 							<h6>Crop Image</h6>							
 							<img class="image2 hidden img-fluid croppr" id="croppr" alt="Image will shown here" />
@@ -131,12 +140,21 @@
 						<div class="pattren_img d-block mx-auto border-1 p-1">
 							<canvas id="c" width="500" height="500"></canvas>
 						</div>
+						<div class="d-block mx-auto">
+						
+						<button id="preview" class="btn btn-custom mt-2  ">Preview</button>			
+						<button id="addText" class="btn btn-custom mt-2  ">Add Text</button>	
+						<select name="getFonts" id="getFonts" class="d-none form-control">
+						
+						</select>		
+						
+						</div>
 								<!-- <canvas id="back_img" width="500" height="500"></canvas> -->
 					</div>
 					<div class="col-sm-6 result1 d-none mt-5">
 						<canvas id="final_image" class="d-block mx-auto" width="500" height="500"></canvas>
 						<h4 class="text-center result-h4">Final Result</h4>
-						<button id="download" class="btn btn-custom mt-2 d-block mx-auto">Download</button>			
+						<button id="download" class="btn btn-custom mt-2 d-block mx-auto d-none">Download</button>			
 					</div>
 
 				</div>
@@ -164,6 +182,7 @@
 			<div class="row">
 			<div class="col-sm-6 "><p class="text-white text-md-left text-center font-weight-bold">CSI@2020 All rights reserved</p></div>
 			<div class="col-sm-6 "> <p class="text-md-right text-center text-white font-weight-bold">TEST</p></div>
+			<div class="col-sm-6 "> <span class="context-menu-one btn btn-neutral">right click me</span></div>
 			</div>
 		</div>
 	</footer>
@@ -177,6 +196,10 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.1.0/fabric.min.js" ></script>
 	<!-- <script src="js/croppr.min.js"></script> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.9/cropper.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.7.1/jquery.contextMenu.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-contextmenu/2.7.1/jquery.ui.position.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
+	<script src="js/fontObserver.js"></script>
     <script>
    
 
@@ -186,42 +209,71 @@
 	var background_img_src = "";
 	var cropInstance = {};
         $(".custom-file-input").on("change", function() {
-        var fileName = $(this).val().split("\\").pop();
-        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+        // var fileName = $(this).val().split("\\").pop();
+        // $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
         });
     $('.file2').on('change', function() {
-	  var reader = new FileReader(),
 		imageSelector = $(this).data('image-selector');
 		console.log(imageSelector);
 		console.log(this.files);
-		console.log(reader);
-	  if (this.files[0]) {
-		reader.onload = function(e) {
-			console.log("add");
-			//imageSelector.cropper("destroy");
-		  imageIsLoaded(e, imageSelector);
-		  
-		};
-		reader.readAsDataURL(this.files[0]);
+		// console.log(reader);
+	  if (this.files[0] && this.files) {
+		for (var i = 0; i < this.files.length; i++) {
+	  		var reader = new FileReader();
+            var file = this.files[i];
+			if (!file.type.match('image')) continue;
+
+			reader.onload = function(e) {
+				//imageSelector.cropper("destroy");
+				console.log("hey");
+			imageIsLoaded(e, imageSelector);
+			
+			};
+			reader.readAsDataURL(this.files[i]);
+		}
 		
 	  }
 	 
 	});
 	function imageIsLoaded(e, imageSelector) {
-
-		
-
-		$(imageSelector).attr('src', e.target.result);
+	
 		img_src = e.target.result;
-		console.log(img_src);
+	
+		//Researching images load
+		$(".uploaded_images").append(`
+		<div class="imagge" style="display:inline;position:relative">
+		<button type="button" class="close" data-dismiss="imagge" style="position:absolute;top:-30px;right:0px" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+		<img src='${img_src}' class='img_fluid img-thumbnail'></div>
+
+		`);
+	
+		$(".uploaded_images").addClass("jumbotron");
+		$(".uploaded_images img").click(function(){
+		// $(this).attr("src");
+		$('.uploaded_images img').removeClass("selected");
+		$(this).addClass("selected");
+		console.log(this)
+		//Old Code
+		$(imageSelector).attr('src', $(this).attr("src"));
 		$(imageSelector).removeClass('hidden');
-		$("#uploaded_img").removeClass('d-none');
+		 $("#uploaded_img").removeClass('d-none');
 		var cropperImage = document.getElementById('croppr');
+		// var cropperImage = this;
 		if(!jQuery.isEmptyObject(cropInstance)){
 			cropInstance.destroy();
 		}
 		console.log(cropInstance);
 		initCrop(cropperImage);
+	})
+		$("button.close").click(function(){
+			$(this).parent().remove();
+			if($(".uploaded_images img").length === 0){
+				$(".uploaded_images").removeClass("jumbotron")
+			}
+		})
+		//Researching images loading closed
 	
 		  
 	}
@@ -267,12 +319,47 @@ var c1 = new fabric.Canvas('c1');
 var canvas = new fabric.Canvas('c');
 var coordinates = {};
 var finalCanvas = document.getElementById("final_image");
-canvas.on("after:render", function(){
-	$(".result1").removeClass("d-none");
+var HideControls = {
+            'tl':true,
+            'tr':false,
+            'bl':true,
+            'br':true,
+            'ml':true,
+            'mt':true,
+            'mr':true,
+            'mb':true,
+            'mtr':true
+        };
+canvas.on("after:render", function(e){
+console.log("do something");
 	var ao = canvas.getActiveObject();
-	console.log(ao);
-  if (ao) {
-    var bound = ao.getBoundingRect();
+	//console.log(ao);
+	if(ao){
+		if(ao.type === "i-text"){
+			console.log("show select");
+			// const res = await fetchFonts();
+					// console.log(res.data);
+					console.log(ao.fontFamily);
+					$("#getFonts").removeClass("d-none");
+		}
+		else{
+			console.log("hide select")
+			$("#getFonts").addClass("d-none");
+		}
+	}
+	else{
+		$("#getFonts").addClass("d-none");
+
+	}
+	// if(e.target.get('type') === "i-text"){
+	// 	console.log("show select")
+	// }
+	// else{
+	// 	console.log("hide")
+	// }
+	// console.log(ao);
+//   if (ao) {
+//     var bound = ao.getBoundingRect();
 
     // canvas.contextContainer.strokeRect(
     //   bound.left,
@@ -280,33 +367,56 @@ canvas.on("after:render", function(){
     //   bound.width,
     //   bound.height
     // );
-	coordinates=bound;
+	// coordinates=bound;
     // console.log(bound);
-			 ftx = finalCanvas.getContext("2d");
-			 ftx.clearRect(0, 0, finalCanvas.width, finalCanvas.height);
-			 backFinalImg = new Image();
-			 aboveFinalImg = new Image();
-			 backFinalImg.src = background_img_src;
-			 backFinalImg.onload = function(){
-				ftx.globalAlpha = 1;
-				ftx.drawImage(backFinalImg, 0, 0, 500, 500);
-				ftx.save();
-				aboveFinalImg.src = cropped_url.toDataURL();
-
-				aboveFinalImg.onload = function(){
-					// ftx.globalAlpha = 1;
-
-					if(ao.angle){
-						//ftx.translate(coordinates.width/2, coordinates.height/2);
-						ftx.rotate(ao.angle * Math.PI/180);
-
-					}
-					ftx.drawImage(aboveFinalImg, coordinates.left, coordinates.top, coordinates.width, coordinates.height);
-					ftx.restore();
-				}
-			 }
-  }		
+			 
+//   }		
 })
+canvas.on('selection:created',function(e){
+	console.log("created")
+    addDeleteBtn(e.target.oCoords.tr.x, e.target.oCoords.tr.y);
+	console.log(e.target.get('type'));
+});
+canvas.on('selection:cleared', function() {
+  console.log("no selection")
+});
+canvas.on('selection:updated', function() {
+	console.log("updated")
+});
+canvas.on('mouse:down',function(e){
+    if(!canvas.getActiveObject())
+    {
+        $(".deleteBtn").remove(); 
+    }
+});
+
+canvas.on('object:modified',function(e){
+    addDeleteBtn(e.target.oCoords.tr.x, e.target.oCoords.tr.y);
+});
+canvas.on('object:scaling',function(e){
+    $(".deleteBtn").remove(); 
+});
+canvas.on('object:moving',function(e){
+    $(".deleteBtn").remove(); 
+});
+canvas.on('object:rotating',function(e){
+    $(".deleteBtn").remove(); 
+});
+$(document).on('click',".deleteBtn",function(){
+    if(canvas.getActiveObject())
+    {
+        canvas.remove(canvas.getActiveObject());
+        $(".deleteBtn").remove();
+    }
+});
+
+function addDeleteBtn(x, y){
+    $(".deleteBtn").remove(); 
+    var btnLeft = x-10;
+    var btnTop = y-10;
+    var deleteBtn = '<img src="https://img.icons8.com/office/16/000000/close-window.png" class="deleteBtn" style="position:absolute;top:'+btnTop+'px;left:'+btnLeft+'px;cursor:pointer;width:20px;height:20px;"/>';
+    $(".canvas-container").append(deleteBtn);
+}
 	function mergeFabric(){
 			cropped_url = cropInstance.getCroppedCanvas();
 			fabric.Image.fromURL(cropped_url.toDataURL(), function(oImg) {
@@ -321,16 +431,18 @@ canvas.on("after:render", function(){
 			$(".result").removeClass("d-none");
 			$(".result3").removeClass("d-none");
 			$("#result4").removeClass("d-none");
-			$("#merger_fabric").addClass("d-none");
+			// $("#merger_fabric").addClass("d-none");
 			console.log(cropInstance);
 		  	cropped_url = cropInstance.getCroppedCanvas();
 			  console.log(cropped_url.toDataURL());
 			//   console.log(imageData); 
 			  console.log(cropped_url)
 			var pattren_img = $(".pattren_img");
+			
 			pattren_img.css("background-image", "url("+background_img_src+")");
 			console.log(cropped_url);
 			fabric.Image.fromURL(cropped_url.toDataURL(), function(oImg) {
+				oImg.setControlsVisibility(HideControls);
 				canvas.add(oImg);
 			});
 			}	
@@ -340,21 +452,163 @@ canvas.on("after:render", function(){
 	  };
 
 	
+	  $(function() {
+        $.contextMenu({
+            selector: '.upper-canvas', 
+			events : {
+				preShow: function (){
+					console.log("pre");
+				},
+				show: function(){
+					console.log("show")
+				}
+			},
+			beforeOpen: function (event, ui) {
+				console.log(event)
+	        var clickPoint = new fabric.Point(event.offsetX, event.offsetY);
+			console.log(clickPoint);
+			},
+            callback: function(key, options) {
+                var m = "clicked: " + key;
+                window.console && console.log(m) || alert(m); 
+            },
+            items: {
+                "edit": {name: "Delete", icon: "delete"},
+                "cut": {name: "Bring to Front", icon: "cut"},
+               copy: {name: "Bring to Back", icon: "copy"},
+                "paste": {name: "Send Backwards", icon: "paste"},
+                "delete": {name: "Delete", icon: "delete"},
+                "sep1": "---------",
+                "quit": {name: "Quit", icon: function(){
+                    return 'context-menu-icon context-menu-icon-quit';
+                }}
+            }
+        });
+
+        $('.context-menu-one').on('click', function(e){
+            console.log('clicked', this);
+        })    
+    });
 
 	$("#download1").click(function(){
 
 	})
-	$('#download').click(function() {
-			 console.log(finalCanvas.toDataURL());
-	 		 download(finalCanvas.toDataURL(),"fabric.png","image/png");
-			//  window.open(canvas.toDataURL('png'))
+	$('#preview').click(function() {
+			ftx = finalCanvas.getContext("2d");
+			 ftx.clearRect(0, 0, finalCanvas.width, finalCanvas.height);
+			 backFinalImg = new Image();
+			 aboveFinalImg = new Image();
+			 backFinalImg.src = background_img_src;
+			 backFinalImg.onload = function(){
+				ftx.globalAlpha = 1;
+				ftx.drawImage(backFinalImg, 0, 0, 500, 500);
+				ftx.save();
+				aboveFinalImg.src = canvas.toDataURL();
+
+				aboveFinalImg.onload = function(){
+					// ftx.globalAlpha = 1;
+					ftx.drawImage(aboveFinalImg, 0, 0, 500, 500);
+					ftx.restore();
+				}
+			 }
+			 //console.log(finalCanvas.toDataURL());
+			 //window.open(finalCanvas.toDataURL('png'))
+			//  $(this).addClass("d-none");
+			 $("#download").removeClass("d-none");
+			 $(".result1").removeClass("d-none");
 		  });
+
+		  $("#addText").click(function(){
+			var textOptions = { 
+				fontSize:16, 
+				left:20, 
+				top:20, 
+				radius:10, 
+				borderRadius: '25px', 
+				hasRotatingPoint: true 
+				};
+
+				var textObject = new fabric.IText('Enter text here...', textOptions);
+				textObject.on("selected", async function(){
+					console.log("text selected");
+
+				})
+
+				canvas.add(textObject).setActiveObject(textObject);
+				canvas.renderAll()
+
+		  })
+
+
+		  //Google Fonts
+		  const FONTAPIKEY = "AIzaSyDzEO5-XXzj6CpXhMQoOmwilS0nPTWda08";
+		  function fetchFonts(){
+			const response = axios.get("https://www.googleapis.com/webfonts/v1/webfonts?key=" + FONTAPIKEY);
+			return response;
+		  }
+		
+
+	$("#download").click(function(){
+	 		  download(finalCanvas.toDataURL(),"fabric.png","image/png");
+	})
+	let fontsData = {};
+	  $(document).ready(async function(){
+		   fontsData = await fetchFonts();
+		   $.each(fontsData.data.items, function(key, item){
+					$("#getFonts").append(`<option value="${item.family}">${item.family}</option>`);
+			})
+		  })
+		  $("#getFonts").change(function(){
+			let selectedFont = $(this).val();
+			// console.log(selectedFont)
+			WebFontConfig = {
+			google: { families: [ selectedFont ]
+				}
+			};
+			(function() {
+				var wf = document.createElement('script');
+				wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+					'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				wf.type = 'text/javascript';
+				wf.async = 'true';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(wf, s);
+			})();
+
+			var ao = canvas.getActiveObject();
+			if(ao.type === "i-text"){
+				loadAndUse(selectedFont);
+			}
+		  })
+		  function loadAndUse(font) {
+			var myfont = new FontFaceObserver(font)
+			myfont.load()
+				.then(function() {
+				// when font is loaded, use it.
+				canvas.getActiveObject().set("fontFamily", font);
+				canvas.requestRenderAll();
+				}).catch(function(e) {
+				console.log(e)
+				alert('font loading failed ' + font);
+				});
+			}
+	</script>
 	
-	
-
-
-
-    </script>
+	<script type="text/javascript">
+	// WebFontConfig = {
+  	// google: { families: [ 'Aclonica', 'Acme', 'Alegreya' ]
+  	// 	}
+	// };
+    //   (function() {
+    //     var wf = document.createElement('script');
+    //     wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    //         '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    //     wf.type = 'text/javascript';
+    //     wf.async = 'true';
+    //     var s = document.getElementsByTagName('script')[0];
+    //     s.parentNode.insertBefore(wf, s);
+    //   })();
+</script>
 </body>
 </html>    
 
